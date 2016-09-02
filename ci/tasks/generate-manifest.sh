e47b2d1c71a05ecc2cab8bd0bf248210cae43428
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-02 15:39:35 -0700

    Update variable name.
    [#128000493]

--- ci/tasks/generate-manifest.sh
+++ ci/tasks/generate-manifest.sh
@@ -11,10 +11,10 @@ omg-cli/omg-linux register-plugin \
   -pluginpath omg-cli-product-bundle/$PRODUCT_PLUGIN
 
 omg-cli/omg-linux deploy-product \
-  --bosh-url $(vault read --field=bosh-url $VAULT_HASH_HOST) \
-  --bosh-port $(vault read --field=bosh-port $VAULT_HASH_HOST) \
-  --bosh-user $(vault read --field=bosh-user $VAULT_HASH_HOST) \
-  --bosh-pass $(vault read --field=bosh-pass $VAULT_HASH_HOST) \
+  --bosh-url $(vault read --field=bosh-url $VAULT_HASH_HOSTVARS) \
+  --bosh-port $(vault read --field=bosh-port $VAULT_HASH_HOSTVARS) \
+  --bosh-user $(vault read --field=bosh-user $VAULT_HASH_HOSTVARS) \
+  --bosh-pass $(vault read --field=bosh-pass $VAULT_HASH_HOSTVARS) \
   --ssl-ignore \ 
   $PRODUCT_PLUGIN
   --cf-release-version $(<cf-release/version)
@@ -28,7 +28,7 @@ omg-cli/omg-linux deploy-product \
   --print-manifest \
   --vault-active \
   --vault-domain $VAULT_ADDR \
-  --vault-hash-host $VAULT_HASH_HOST \
+  --vault-hash-host $VAULT_HASH_HOSTVARS \
   --vault-hash-ip $VAULT_HASH_IP \
   --vault-hash-keycert $VAULT_HASH_KEYCERT \
   --vault-hash-password $VAULT_HASH_PASSWORD \
