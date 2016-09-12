cee4815c1baeb28770ff974bd0457721cbf63e18
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-12 12:01:05 -0700

    Add groups. Feed versions into deploy job. Optimizations.
    [#128000493]

--- ci/tasks/get-versions-from-plugin.sh
+++ ci/tasks/get-versions-from-plugin.sh
@@ -1,26 +1,20 @@
 #!/bin/bash -e
 
-if [[ -z $PRODUCT ]] ; then
-  printf "ERROR: \$PRODUCT not defined\n"
-  exit 1
-fi
-
-if [[ -z $OUTPUT_DIR ]] ; then
-  printf "ERROR: \$OUTPUT_DIR not defined\n"
-  exit 1
-fi
-
 chmod +x omg-cli/omg-linux
 
 omg-cli/omg-linux register-plugin \
   -type product \
   -pluginpath omg-cli-product-bundle/$PRODUCT_PLUGIN
 
-PRODUCT_NAME=$(printf $PRODUCT_PLUGIN | cut -d- -f1)
+PRODUCT_NAME=$(printf ${PRODUCT_PLUGIN%%-*})
 
 omg-cli/omg-linux product-meta $PRODUCT_NAME | \
-  grep ^pivotal-$PRODUCT | awk '{print $NF}' > $OUTPUT_DIR/version
+  awk '/^pivotal-/ {printf $NF}' > $OUTPUT_DIR/product_version
 
-printf "Version $(<$OUTPUT_DIR/version)\n"
+omg-cli/omg-linux product-meta $PRODUCT_NAME | \
+  awk '/^stemcell:/ {printf $NF}' > $OUTPUT_DIR/stemcell_version
+
+printf "Pivotal $PRODUCT_NAME version: $(<$OUTPUT_DIR/product_version)\n"
+printf "Stemcell version: $(<$OUTPUT_DIR/stemcell_version)\n"
 
 #eof
