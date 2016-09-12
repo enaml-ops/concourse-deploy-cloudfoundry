58de9dd21a2e823ad199fc26edf68346d5169824
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-12 13:13:16 -0700

    Fix fly.
    [#128000493]

--- ci/tasks/update-pipeline.sh
+++ ci/tasks/update-pipeline.sh
@@ -3,9 +3,9 @@
 curl -SsL -u "$CONCOURSE_USER:$CONCOURSE_PASS" "$CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux" > fly
 chmod +x fly
 
-./fly login -t here -c "$CONCOURSE_URL" -u "$CONCOURSE_USER" -p "$CONCOURSE_PASS"
-./fly login -t here get-pipeline -p $PIPELINE_NAME > $PIPELINE_NAME.yml
-./fly login -t here set-pipeline -p $PIPELINE_NAME -c $PIPELINE_NAME.yml \
+./fly -t here login -c "$CONCOURSE_URL" -u "$CONCOURSE_USER" -p "$CONCOURSE_PASS"
+./fly -t here get-pipeline -p $PIPELINE_NAME > $PIPELINE_NAME.yml
+./fly -t here set-pipeline -p $PIPELINE_NAME -c $PIPELINE_NAME.yml \
   -v elastic_runtime_version=$(<product_version) \
   -v stemcell_version=$(<stemcell_version)
 
