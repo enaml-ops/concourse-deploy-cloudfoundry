90320d57d864240969ae2f5c010b62605b81e981
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-12 14:03:39 -0700

    Fix ert and stemcell variable names.  Add periodic tests.
    [#128000493]

--- ci/tasks/update-pipeline.sh
+++ ci/tasks/update-pipeline.sh
@@ -6,8 +6,8 @@ chmod +x fly
 ./fly -t here login -c "$CONCOURSE_URL" -u "$CONCOURSE_USER" -p "$CONCOURSE_PASS"
 ./fly -t here get-pipeline -p $PIPELINE_NAME > $PIPELINE_NAME.yml
 ./fly -t here set-pipeline -p $PIPELINE_NAME -c $PIPELINE_NAME.yml \
-  -v elastic_runtime_version=$(<versions/product_version) \
-  -v stemcell_version=$(<versions/stemcell_version) <<EOF
+  -v elastic-runtime-version=$(<versions/product_version) \
+  -v stemcell-version=$(<versions/stemcell_version) <<EOF
 y
 EOF
 
