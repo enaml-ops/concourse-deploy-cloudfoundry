1133bebd543b9bc2000db1e0e8d2a290dfea9b68
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-12 13:17:17 -0700

    Fix fly set-pipeline
    [#128000493]

--- ci/tasks/update-pipeline.sh
+++ ci/tasks/update-pipeline.sh
@@ -6,7 +6,9 @@ chmod +x fly
 ./fly -t here login -c "$CONCOURSE_URL" -u "$CONCOURSE_USER" -p "$CONCOURSE_PASS"
 ./fly -t here get-pipeline -p $PIPELINE_NAME > $PIPELINE_NAME.yml
 ./fly -t here set-pipeline -p $PIPELINE_NAME -c $PIPELINE_NAME.yml \
-  -v elastic_runtime_version=$(<product_version) \
-  -v stemcell_version=$(<stemcell_version)
+  -v elastic_runtime_version=$(<versions/product_version) \
+  -v stemcell_version=$(<versions/stemcell_version) <<EOF
+y
+EOF
 
 #eof
