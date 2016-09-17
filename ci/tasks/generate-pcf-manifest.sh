b8e68be26f6327b2a2e5c079aed87d28877351e4
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-16 17:46:57 -0700

    Update vault hash.
    [#128000493]

--- ci/tasks/generate-pcf-manifest.sh
+++ ci/tasks/generate-pcf-manifest.sh
@@ -11,10 +11,10 @@ if [[ $SKIP_HAPROXY == "false" ]]; then
 fi
 
 omg-cli/omg-linux deploy-product \
-  --bosh-url $(vault read -field=bosh-url $VAULT_HASH_HOSTVARS) \
-  --bosh-port $(vault read -field=bosh-port $VAULT_HASH_HOSTVARS) \
-  --bosh-user $(vault read -field=bosh-user $VAULT_HASH_HOSTVARS) \
-  --bosh-pass $(vault read -field=bosh-pass $VAULT_HASH_HOSTVARS) \
+  --bosh-url $(vault read -field=bosh-url $VAULT_HASH_MISC) \
+  --bosh-port $(vault read -field=bosh-port $VAULT_HASH_MISC) \
+  --bosh-user $(vault read -field=bosh-user $VAULT_HASH_MISC) \
+  --bosh-pass $(vault read -field=bosh-pass $VAULT_HASH_MISC) \
   --print-manifest \
   --ssl-ignore \
   $PRODUCT_PLUGIN \
