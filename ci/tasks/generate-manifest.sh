ce3b5d0bd444e6bc7d623c070e804637f366b9d8
Michael Jacobi <mjacobi@Michaels-MacBook.local>
2016-09-20 16:53:27 -0700

    Aggregate resource downloads.  Variabilize more.
    Cleanup.
    
    [#129368835]

--- ci/tasks/generate-manifest.sh
+++ ci/tasks/generate-manifest.sh
@@ -6,11 +6,15 @@ omg-cli/omg-linux register-plugin \
   -type product \
   -pluginpath omg-product-bundle/$PRODUCT_PLUGIN
 
+if [[ $SKIP_HAPROXY == "false" ]]; then
+  HAPROXY_FLAG="--skip-haproxy=false"
+fi
+
 omg-cli/omg-linux deploy-product \
-  --bosh-url $(vault read -field=bosh-url $VAULT_HASH_HOSTVARS) \
-  --bosh-port $(vault read -field=bosh-port $VAULT_HASH_HOSTVARS) \
-  --bosh-user $(vault read -field=bosh-user $VAULT_HASH_HOSTVARS) \
-  --bosh-pass $(vault read -field=bosh-pass $VAULT_HASH_HOSTVARS) \
+  --bosh-url $(vault read -field=bosh-url $VAULT_HASH_MISC) \
+  --bosh-port $(vault read -field=bosh-port $VAULT_HASH_MISC) \
+  --bosh-user $(vault read -field=bosh-user $VAULT_HASH_MISC) \
+  --bosh-pass $(vault read -field=bosh-pass $VAULT_HASH_MISC) \
   --print-manifest \
   --ssl-ignore \
   $PRODUCT_PLUGIN \
@@ -21,13 +25,8 @@ omg-cli/omg-linux deploy-product \
   --cf-mysql-release-version $(<cf-mysql-release/version) \
   --cflinuxfs2-release-version $(<cflinuxfs2-release/version) \
   --stemcell-version $(<stemcells/version) \
+  $HAPROXY_FLAG \
   --infer-from-cloud \
-  --nfs-disk-type large \
-  --nfs-vm-type medium \
-  --mysql-disk-type large \
-  --diego-brain-disk-type large \
-  --diego-cell-disk-type large \
-  --diego-cell-vm-type large.cpu \
   --vault-active \
   --vault-domain $VAULT_ADDR \
   --vault-hash-host $VAULT_HASH_HOSTVARS \
--- ci/tasks/update-pipeline.sh
+++ ci/tasks/update-pipeline.sh
@@ -7,15 +7,21 @@ chmod +x fly
 ./fly -t here get-pipeline -p $PIPELINE_NAME > $PIPELINE_NAME.yml
 
 sed -i.original "
-  /^- name: elastic-runtime$/,/product_version:/ {
-    s,\(product_version:\).*,\1 $(<versions/product_version),
-  }
 
-  /^- name: stemcells$/,/product_version:/ {
-    s,\(product_version:\).*,\1 \"$(<versions/stemcell_version)\",
+  /^resources:$/,/^resource_types:$/ {
+
+    /^- name: $PRODUCT$/,/product_version:/ {
+      s,\(product_version:\).*,\1 $(<versions/product_version),
+    }
+
+    /^- name: stemcells$/,/product_version:/ {
+      s,\(product_version:\).*,\1 \"$(<versions/stemcell_version)\",
+    }
+
   }
 
   s,\(STEMCELL_VERSION:\).*,\1 \"$(<versions/stemcell_version)\",
+
 " $PIPELINE_NAME.yml
 
 ./fly -t here set-pipeline -n -p $PIPELINE_NAME -c $PIPELINE_NAME.yml
